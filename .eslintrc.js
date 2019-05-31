a91d4a814d5de68f79cbc925b06be79329b02136
Christopher Bonhage <chris@mix.com>
2019-05-31 12:23:07 -0400

    Update NPM packaging

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+      "commonjs": true,
+      "es6": true,
+      "node": true,
+      "mocha": true
+  },
+  "parserOptions": {
+      "ecmaVersion": 2018
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+  }
+};
